217b478d9e5a2d57eb699e0bf3d567b37389e113
Mudra-Katrin <114252480+Mudra-Katrin@users.noreply.github.com>
2022-12-06 00:15:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+string[] manualInputArray = GetArrayFromConsole("Введите массив данных разделяя , : ");
+Console.WriteLine($"{ArrayToString(manualInputArray)}");
+
+FindStringBySizeInArray(manualInputArray);
+
+string[] GetArrayFromConsole(string message = "")
+{
+    Console.Write(message);
+    return Console.ReadLine()!.Split(",");
+}
+
+void FindStringBySizeInArray(string[] arr2)
+{
+    Console.WriteLine("Результат: ");
+    string[] newarr = new string[arr2.Length];
+ 
+    for (int i = 0; i < arr2.Length; i++)
+    {
+        if (arr2[i].Length <= 3)
+        {
+            newarr[i] = arr2[i];
+            
+        }
+       
+    }
+    Console.Write($"{ArrayToString(newarr)}");
+    
+}
+string ArrayToString(string[] array)
+{
+
+    string text = $"[\"{String.Join("\",\"", array)}\"]";
+    return text;
+}
